7dd668e566f1e320ec0e4052c6782b7283db382f
Suphakrit <164913096+a456789123102@users.noreply.github.com>
2024-11-07 08:08:25 +0000

    spiralMatrix

--- Leetcode/54.Spiral Matrix.js
+++
@@ -1,3 +0,0 @@
-var spiralOrder = function(matrix) {
-    
-};
---
+++ Leetcode/54.SpiralMatrix.js
@@ -0,0 +1,40 @@
+var spiralOrder = function (matrix) {
+  let ans = [];
+  let left = 0;
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let right = matrix[0].length-1;
+  let targets = matrix.length * matrix[0].length; //3x3 = 9
+
+  while (ans.length < targets) {
+    //top left to top right
+    for (let i = left; i <= right && ans.length < targets; i++) {
+      ans.push(matrix[top][i]);
+    }
+    top++; //increase top
+
+    // top right to bottom right
+    for (let i = top; i <= bottom && ans.length < targets; i++) {
+      ans.push(matrix[i][right]);
+    }
+    right--; //decrease right
+
+
+    //bottom right to bottom left
+    for (let i = right; i >= left && ans.length < targets; i--) {
+      ans.push(matrix[bottom][i]);
+    }
+    bottom--; //decrease bottom
+
+    //bottom left to top left
+    for (let i = bottom; i >= top && ans.length < targets; i--) {
+      ans.push(matrix[i][left]);
+    }
+    left++; //increase left
+  }
+
+  return ans;
+};
+
+console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+console.log(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
