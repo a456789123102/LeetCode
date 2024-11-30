2d9a00847d92b9f3e9e6f0152437932e7418327a
456789123102 <aize.gong@gmail.com>
2024-11-30 13:13:08 +0700

    11

---
+++ Leetcode/11.ContainerWithMostWater.js
@@ -0,0 +1,27 @@
+var maxArea = function (height) {
+  if (height.length === 1) return height;
+  let maxSum = -Infinity;
+  let left = 0;
+  let right = height.length - 1;
+
+  while(left < right){
+    const minH = Math.min(height[left],height[right]);
+    const width = right - left;
+    console.log(`widgth = ${width}`)
+    maxSum = Math.max(maxSum,width*minH);
+    if(height[left] < height[right]){
+        left ++;
+        console.log(`plus left ${left} to val = ${height[left]}`)
+    }else{
+        right--;
+        console.log(`reduce right ${right} to val = ${height[right]}`)
+    }
+
+  
+  }
+return maxSum
+}
+
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1,2,4,3]));
+console.log(maxArea([8,7,2,1]))
