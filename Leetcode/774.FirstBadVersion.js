3fcf13de970fe183b9c215c8fe6f39dfcc801b1b
456789123102 <aize.gong@gmail.com>
2025-03-22 20:46:39 +0700

    774

---
+++ Leetcode/774.FirstBadVersion.js
@@ -0,0 +1,16 @@
+var solution = function(isBadVersion) {
+    return function(n) {
+        let left = 0;
+        let right = n;
+        
+        while(left < right){
+            let mid = Math.floor((left + right) / 2);
+            if(isBadVersion(mid)){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left
+    };
+};
